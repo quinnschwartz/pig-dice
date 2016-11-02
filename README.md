aff56a92b828793719a8e589799ad450acb437de
Michael Andrade and Quinn Schwartz <michael@>
2016-11-02 11:12:44 -0700

    complete specs 2 and 3.

--- README.md
+++ README.md
@@ -1,18 +1,32 @@
 Spec List
 
-0) let's play button which requires a number of players
+0) let's play button which requires a number of players from 1-4
+in: enter number 2 in number box and click start game
+out: player 1 score and player 2 score appear, game starts
 
 1) if the player hits the "roll" button a random number from 1-6 is returned.
+in: click roll button
+out: 6, 6, 4, 2, 3, etc.
 
 2) that number is added to the turn score.
+in: clicking roll button
+out: (subtotal 24: 6, 6, 4, 3, 5)
 
 3) If it's 1, the turn score resets and the turn is over.
+in: clicking roll button
+out: 5, 2, 3, 1-TOO GREEDY YOURE out
 
 4) if the user hits the "hold" button then their turn score is added to their total score and the turn is over.
+in: (subtotal 24: 6, 6, 4, 3, 5), click hold button
+out: (subtotal 0: ), player Total Score: 24
 
 5) if there are multiple players, the next player in order starts their turn and the process is repeated.
+in: click hold button OR click roll button and roll 1
+out: Player 2 Current Turn Score: (subtotal 0: )
 
 6) when a player's total reaches 100 or more the game is over and a happy pig image is displayed.
+in: player X total score >= 100
+out: PLAYER X WINS OINK OINK
 
 7)instructions sidebar
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,43 @@
 //backend
+Player = function(name) {
+  this.name = name;
+  this.score = 0;
+};
+
+Game = function(player1, player2, player3, player4) {
+  this.players = [player1, player2, player3, player4];
+}
+
+
+
 var rollDie = function() {
   return Math.ceil(Math.random() * 6);
 }
 
 //frontend
 $(document).ready(function() {
+    var subTotal = 0;
   $("form#startGame").submit(function(event) {
     event.preventDefault();
 
-    // var numberOfPlayers = $("input#playersNumber").val();
+    var player1Name = $("input#player1NameInput").val();
+    var player2Name = $("input#player2NameInput").val();
+    var player3Name = $("input#player3NameInput").val();
+    var player4Name = $("input#player4NameInput").val();
+    var newGame = new Game(player1Name, player2Name, player3Name, player4Name);
+    console.log(newGame);
   });
+
   $("button#rollButton").click(function(event) {
   var result = rollDie();
   if (result === 1) {
   $("ol#currentTurn").append("<li>" + result + "</li>  <h3>Too Greedy, score 0 this turn!</h3>");
+  subTotal = 0;
+  $("#currentTurnScore").text(subTotal);
   } else {
   $("ol#currentTurn").append("<li>" + result + "</li>");
+  subTotal += result;
+  $("#currentTurnScore").text(subTotal);
   }
-
   });
 });
